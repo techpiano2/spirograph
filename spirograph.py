50c1fe305170be7abad927e8ab54774dcdeabe02
techpiano2 <techpiano2@gmail.com>
2021-11-06 18:10:02 -0400

    Adjusted all methods to take a variable t-range and added a slider for the max t

--- spirograph.py
+++ spirograph.py
@@ -33,33 +33,35 @@ len = 200
 # Low delta can be difficult for the processor
 delta = 0.001
 
-t = np.arange(0, len, delta)
+tRange = np.arange(0, len, delta)
 
 
-def hipo_draw(k, k2, h, p):
-    '''
+def hipo_draw(k, k2, h, p, t):
+    """
     Draws the hypo(cycloid/trochoid) for two circles
     :param k: The ratio of the first (innermost) circle's radius to the second circle's radius
     :param k2: The ratio of the first circle's radius to the third (outermost) circle's radius
     :param h: The distance from the center of the third (outermost) circle to the drawing point
     :param p: The ratio of the arc length for rotation alpha of the innermost circle to the travelled arc length of the outermost circle
+    :param t: The parametric range to draw the graph for
     :returns: The x and y coordinates for the graph drawn using the specified parameters
-    '''
+    """
     r = R / k
     x = (R - r) * np.cos(t) + h * np.cos(t - R * t / r)
     y = (R - r) * np.sin(t) + h * np.sin(t - R * t / r)
     return x, y
 
 
-def hipo2_draw(k, k2, h, p):
-    '''
+def hipo2_draw(k, k2, h, p, t):
+    """
     Draws the hypo(cycloid/trochoid) for three circles
     :param k: The ratio of the first (innermost) circle's radius to the second circle's radius
     :param k2: The ratio of the first circle's radius to the third (outermost) circle's radius
     :param h: The distance from the center of the third (outermost) circle to the drawing point
     :param p: The ratio of the arc length for rotation alpha of the innermost circle to the travelled arc length of the outermost circle
+    :param t: The parametric range to draw the graph for
     :returns: The x and y coordinates for the graph drawn using the specified parameters
-    '''
+    """
     r = R / k
     r2 = r / k2
     x = (R + r) * np.cos(t) - (r - r2) * np.cos(-t) + h * np.cos(t + r * t / r2)
@@ -68,30 +70,32 @@ def hipo2_draw(k, k2, h, p):
     return x, y
 
 
-def epi_draw(k, k2, h, p):
-    '''
+def epi_draw(k, k2, h, p, t):
+    """
     Draws the epi(cycloid/trochoid) for two circles
     :param k: The ratio of the first (innermost) circle's radius to the second circle's radius
     :param k2: The ratio of the first circle's radius to the third (outermost) circle's radius
     :param h: The distance from the center of the third (outermost) circle to the drawing point
     :param p: The ratio of the arc length for rotation alpha of the innermost circle to the travelled arc length of the outermost circle
+    :param t: The parametric range to draw the graph for
     :returns: The x and y coordinates for the graph drawn using the specified parameters
-    '''
+    """
     r = R / k
     x = (R + r) * np.cos(t) - h * np.cos(t + R * t / r)
     y = (R + r) * np.sin(t) - h * np.sin(t + R * t / r)
     return x, y
 
 
-def epi2_draw(k, k2, h, p):
-    '''
+def epi2_draw(k, k2, h, p, t):
+    """
     Draws the epi(cycloid/trochoid) for three circles
     :param k: The ratio of the first (innermost) circle's radius to the second circle's radius
     :param k2: The ratio of the first circle's radius to the third (outermost) circle's radius
     :param h: The distance from the center of the third (outermost) circle to the drawing point
     :param p: The ratio of the arc length for rotation alpha of the innermost circle to the travelled arc length of the outermost circle
+    :param t: The parametric range to draw the graph for
     :returns: The x and y coordinates for the graph drawn using the specified parameters
-    '''
+    """
     r = R / k
     r2 = R / k2
 
@@ -109,36 +113,40 @@ def epi2_draw(k, k2, h, p):
 
 
 def update(val):
-    '''
+    """
     Function called on the update of any of the sliders, re-draws the image using the selected function
     :param val: The values of the sliders
-    '''
-    x, y = epi2_draw(k_slider.val, k2_slider.val, h_slider.val, p_slider.val)
+    """
+    x, y = epi2_draw(k_slider.val, k2_slider.val, h_slider.val, p_slider.val, np.arange(0, tMax_slider.val, delta))
 
     sp2.set_xdata(x)
     sp2.set_ydata(y)
     fig.canvas.draw_idle()
 
 
-x, y = epi2_draw(k, k2, h, p)
+x, y = epi2_draw(k, k2, h, p, tRange)
 (sp2,) = ax.plot(x, y, linewidth=0.5, color="red", antialiased=True)
 
 
 # Sliders
-h_ax = plt.axes([0.25, 0.07, 0.65, 0.01])
+h_ax = plt.axes([0.25, 0.09, 0.65, 0.01])
 h_slider = Slider(h_ax, "h", valmin=0, valmax=200.0, valinit=h, valstep=1)
 h_slider.on_changed(update)
 
-p_ax = plt.axes([0.25, 0.05, 0.65, 0.01])
+p_ax = plt.axes([0.25, 0.07, 0.65, 0.01])
 p_slider = Slider(p_ax, "p", valmin=-5, valmax=10.0, valinit=p, valstep=0.01)
 p_slider.on_changed(update)
 
-k_ax = plt.axes([0.25, 0.03, 0.65, 0.01])
+k_ax = plt.axes([0.25, 0.05, 0.65, 0.01])
 k_slider = Slider(k_ax, "k", valmin=0, valmax=10.0, valinit=k, valstep=0.01)
 k_slider.on_changed(update)
 
-k2_ax = plt.axes([0.25, 0.01, 0.65, 0.01])
+k2_ax = plt.axes([0.25, 0.03, 0.65, 0.01])
 k2_slider = Slider(k2_ax, "k2", valmin=0.1, valmax=10, valinit=k2, valstep=0.01)
 k2_slider.on_changed(update)
+
+tMax_ax = plt.axes([0.25, 0.01, 0.65, 0.01])
+tMax_slider = Slider(tMax_ax, "t", valmin=0, valmax=500, valinit=tRange[-1], valstep=1)
+tMax_slider.on_changed(update)
 
 plt.show()
